7ca02150dd9de1b00641583cfc6e9cec559c1681
klar-sites <klar.sites@twixly.io>
2026-05-03 20:44:24 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ if (closeDropdown) {
       .map((item) => {
           item.addEventListener('click', (e) => {
             resetMenu();
+            e.currentTarget.classList.add('bg-muted');
             e.currentTarget.setAttribute('aria-selected', true);
             const tag = e.currentTarget.querySelector('div span').innerText;
             const parser = new DOMParser();

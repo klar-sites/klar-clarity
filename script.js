3c79f3649cd4dffd797f723c21fc2da2eb9c5bdb
klar-sites <klar.sites@twixly.io>
2026-05-03 18:37:54 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const closeDropdown = document.querySelector('#closeDropdown');
 if (closeDropdown) {
   setTimeout(() => {
     const allCategories = document.querySelector('#all-categories');
-    [...allCategories.querySelectorAll('button div span'))].map((item) => item.addEventListener('click', (e) => {
-      const tag = e.currentTarget.innerText;
-      console.log(tag);
-    });}, 1000)
+    [...allCategories.querySelectorAll('button div span'))]
+      .map((item) => 
+        item.addEventListener('click', (e) => {
+          const tag = e.currentTarget.innerText;
+          console.log(tag);
+        });}), 1000)
   
   document.querySelector('[aria-label="Filter by Category"]').addEventListener('click', (e) => {
     const button = e.currentTarget; 

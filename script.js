4a4d992e160d80f1ce8a8f86d9333b78c49aee52
klar-sites <klar.sites@twixly.io>
2026-05-03 19:24:45 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ if (closeDropdown) {
       .map((item) => {
           item.addEventListener('click', (e) => {
             const tag = e.currentTarget.querySelector('div span').innerText;
+            svgFilter.insertAfter(e.currentTarget.querySelector('div span'));
+            
             // console.log('asdf', tag)
             setPosts(tag, null);
           });
@@ -83,6 +85,8 @@ if (closeDropdown) {
     }
   });
 }
+
+const svgFilter = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-filter h-3 w-3"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>`;
 
 document.querySelector('[aria-label="Toggle theme"]').addEventListener('click', (e) => {
   try {

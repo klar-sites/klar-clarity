b05c1227051d3bd5b3edfa7130304cb3f4d343cd
klar-sites <klar.sites@twixly.io>
2026-05-03 10:52:35 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function foo() {
   // Option 2: keep arrow, use e.currentTarget
-document.querySelector('[aria-label="Toggle theme"]]').addEventListener('mouseover', (e) => {
+document.querySelector('[aria-label="Toggle theme"]').addEventListener('mouseover', (e) => {
   console.log('d'); // <header> 
 });
   console.log('a');

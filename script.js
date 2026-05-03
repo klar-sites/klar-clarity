b3be2188b63cadc87f838d47a6040b2e511b86cd
klar-sites <klar.sites@twixly.io>
2026-05-03 11:11:48 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.querySelector('[aria-label="Toggle theme"]').addEventListener('click', (e) => {
   try {
-  var d = document.documentElement,
-  c = d.classList;
-  var e = localStorage.getItem('theme');
-  if (!e || e === 'light') {
-    d.style.colorScheme = 'dark';
-    c.remove('light');
-    c.add('dark');
-    localStorage.setItem('theme', 'dark');
-  } else { 
-    d.style.colorScheme = 'light';
-    c.remove('dark');
-    c.add('light');
-    localStorage.setItem('theme', 'light');
+    var d = document.documentElement,
+    c = d.classList;
+    var e = localStorage.getItem('theme');
+    if (!e || e === 'light') {
+      d.style.colorScheme = 'dark';
+      c.remove('light');
+      c.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else { 
+      d.style.colorScheme = 'light';
+      c.remove('dark');
+      c.add('light');
+      localStorage.setItem('theme', 'light');
+    }
+  } catch (e) {
+      console.log(e)
   }
-} catch (e) {
-    console.log(e)
-}
 });

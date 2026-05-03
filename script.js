b66c4df6df3891bd410d89d515dd4dbe0429b6ba
klar-sites <klar.sites@twixly.io>
2026-05-03 14:37:27 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('[aria-label="Filter by Category"]').addEventListener('click', (e) => {
-  const button = e.target;
+  const button = e.currentTarget;
   console.log(e);  
   button.querySelector('svg').classList.add('rotate-180');
   

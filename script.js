9f9834fb8a8b9fcf24bcdde437d99d279d764a80
klar-sites <klar.sites@twixly.io>
2026-05-05 15:51:39 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
 console.log(mobileMenu.classList) 
 mobileMenuToggle.addEventListener('click', (e) => {
-  if (mobileMenu.classList.includes('hidden')) {
-  [...mobileMenu.classList].remove('hidden');  
+  if ([...mobileMenu.classList].includes('hidden')) {
+  mobileMenu.classList.remove('hidden');  
   } else {
     
-  [...mobileMenu.classList].add('hidden');
+  mobileMenu.classList.add('hidden');
   }
 });
 

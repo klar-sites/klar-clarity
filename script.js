751cd931c69ed203b21b029f5f6bb4fd7deec760
klar-sites <klar.sites@twixly.io>
2026-05-05 20:38:02 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ if (closeDropdown) {
               selectedCategory = selectedCategory.querySelector('div span').innerText;
             }
             let selectedTopic = allTopics?.querySelector('[aria-selected="true"]');
-            console.log(allTopics)
+            // console.log(allTopics)
             if (!selectedTopic) {
               selectedTopic = 'all';
             } else {

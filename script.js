a2c13842a74a230a2cc3067b7b4abeec7fcbdfd6
klar-sites <klar.sites@twixly.io>
2026-05-05 15:52:02 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ mobileMenuToggle.addEventListener('click', (e) => {
   mobileMenu.classList.remove('hidden');  
   } else {
     
-  mobileMenu.classList.add('hidden');
+  mobileMenu.classList.remove('hidden');
   }
 });
 

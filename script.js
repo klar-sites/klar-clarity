63f6489de5a9c77da464b77edacd1138ebdfffac
klar-sites <klar.sites@twixly.io>
2026-05-06 09:55:47 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.klar = createKlarClient({
   source: location.host ===
     '' ?
     // 'https://editor.klar.website/static/db.json' : '/klar-clarity/assets/db.json'
-    'http://localhost:5173/db/' + projectId + '.json' : 'http://localhost:5173/db/' + projectId + '.json'
+    `http://localhost:5173/db/${projectId}.json' : 'http://localhost:5173/db/${projectId}.json`
 });
 window.klarSdk = klar;
 

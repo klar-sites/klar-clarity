cbef75a954257ad4f740e357fbc910d2c34f0b1d
klar-sites <klar.sites@twixly.io>
2026-05-14 16:04:59 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -471,23 +471,26 @@ if (closeDropdown) {
 
 const svgFilter = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-filter h-3 w-3"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>`;
 
-document.querySelector('[aria-label="Toggle theme"]').addEventListener('click', (e) => {
-  try {
-    var d = document.documentElement,
-    c = d.classList;
-    var e = localStorage.getItem('theme');
-    if (!e || e === 'light') {
-      d.style.colorScheme = 'dark';
-      c.remove('light');
-      c.add('dark');
-      localStorage.setItem('theme', 'dark');
-    } else { 
-      d.style.colorScheme = 'light';
-      c.remove('dark');
-      c.add('light');
-      localStorage.setItem('theme', 'light');
+// document.querySelector('[aria-label="Toggle theme"]')
+[...document.querySelectorAll('[aria-label="Toggle theme"]')].map((item) => {  
+    item.addEventListener('click', (e) => {
+    try {
+      var d = document.documentElement,
+      c = d.classList;
+      var e = localStorage.getItem('theme');
+      if (!e || e === 'light') { 
+        d.style.colorScheme = 'dark';
+        c.remove('light');
+        c.add('dark');
+        localStorage.setItem('theme', 'dark');
+      } else { 
+        d.style.colorScheme = 'light';
+        c.remove('dark');
+        c.add('light');
+        localStorage.setItem('theme', 'light');
+      }
+    } catch (e) {
+        console.log(e)
     }
-  } catch (e) {
-      console.log(e)
-  }
+  });
 });
